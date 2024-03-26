5ba0452e1b7c6bcf7aee7512b4a6e4940d71e641
samrat87573015 <www.samrat87573015@gmail.com>
2024-03-26 19:37:56 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */ 
+module.exports = { 
+  content: ['*'],   
+  theme: { 
+  extend: {
+    fontFamily: {
+      'Josefin':['Josefin Sans', 'sans-serif']
+    }
+  }, 
+  }, 
+  plugins: [], 
+  }
