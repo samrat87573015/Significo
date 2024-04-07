e5603e8a047ae9ddf2805a0b50090078fafb7439
samrat87573015 <www.samrat87573015@gmail.com>
2024-04-07 15:06:15 +0600

    team area done

--- app.js
+++ app.js
@@ -40,13 +40,37 @@ function heroAnimation(){
 
 heroAnimation()
 
-gsap.to(".slider_item",{
-    scrollTrigger: {
-        trigger: ".scrol_slider_area",
-        start: "top top",
-        end: "bottom bottom",
-        scrub: 2,
-    },
-    xPercent: -200,
-    ease: Power4,
+function scollSliderAnimation(){
+    gsap.to(".slider_item",{
+        scrollTrigger: {
+            trigger: ".scrol_slider_area",
+            start: "top top",
+            end: "bottom bottom",
+            scrub: 2,
+        },
+        xPercent: -200,
+        ease: Power4,
+    })
+}
+scollSliderAnimation();
+
+let teamItem = document.querySelectorAll(".team_item");
+
+teamItem.forEach(function(el){
+    el.addEventListener("mousemove", function(dets){
+        let teamImg = this.querySelector(".team_img");
+        gsap.to(teamImg, {
+            x: gsap.utils.mapRange(0, window.innerWidth, -200, 200, dets.clientX),
+            opacity: 1,
+            ease: Power4,
+            duration: 0.5,
+        })
+    });
+
+    el.addEventListener("mouseleave", function(dets){
+        let teamImg = this.querySelector(".team_img");
+        gsap.to(teamImg, {
+            opacity: 0,
+        })
+    });
 })
